fcaf86c603fefe37f940ad07da3891cde90c0794
George Travis <wewerelikesharks@gmail.com>
2020-03-10 18:57:33 -0700

    added a test for each column entered except auto timestamps for both tables

--- src/test/UsersDao.spec.js
+++ src/test/UsersDao.spec.js
@@ -4,7 +4,7 @@ jest.mock('../main/models/users', () => () => {
   const SequelizeMock = require('sequelize-mock');
   const dbMock = new SequelizeMock();
   return dbMock.define('user', {
-    userId: 2,
+    id: 2,
     username: 'agtravis',
     password: '1234',
     creTs: '2019-01-01 13:30:31',
@@ -15,6 +15,16 @@ jest.mock('../main/models/users', () => () => {
 describe('Test Sequelize Mocking', () => {
   it('Should get value from mock', async () => {
     const user = await UserDAO.getOneUser();
+    expect(user.id).toEqual(2);
+  });
+
+  it('Should get value from mock', async () => {
+    const user = await UserDAO.getOneUser();
     expect(user.username).toEqual('agtravis');
+  });
+
+  it('Should get value from mock', async () => {
+    const user = await UserDAO.getOneUser();
+    expect(user.password).toEqual('1234');
   });
 });
