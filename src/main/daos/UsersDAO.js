c0dc1491a5a12e195f820ae477dffedf63991045
Frantz Felix <46702267+FrantzCFelix@users.noreply.github.com>
2020-03-10 20:42:19 -0700

    Revert "Extra front end"

--- src/main/daos/UsersDAO.js
+++ src/main/daos/UsersDAO.js
@@ -1,16 +1,13 @@
-'use strict';
-
-const Sequelize = require(`sequelize`);
-const sequelize = new Sequelize(`phaser_db`, `root`, `MyPassword`, {
-  host: `127.0.0.1`,
-  dialect: `mysql`,
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('phaser_db', 'root', 'George1!', {
+  host: '127.0.0.1',
+  dialect: 'mysql',
   operatorsAliases: false,
   dialectOptions: {
     dateStrings: true,
-    // eslint-disable-next-line object-shorthand
     typeCast: function(field, next) {
       // for reading from database
-      if (field.type === `DATETIME`) {
+      if (field.type === 'DATETIME') {
         return field.string();
       }
       return next();
@@ -18,20 +15,18 @@ const sequelize = new Sequelize(`phaser_db`, `root`, `MyPassword`, {
   }
 });
 
-const User = require(`../models/users`)(sequelize, Sequelize); // you can do this in many ways
+const User = require('../models/users')(sequelize, Sequelize); //you can do this in many ways
 
 module.exports = {
-  // eslint-disable-next-line object-shorthand
   getOneUser: function() {
     return new Promise((resolve, reject) => {
       User.findOne()
         .then(user => {
-          // eslint-disable-next-line prefer-template
-          console.log(`user value is ` + JSON.stringify(user));
+          console.log('user value is ' + JSON.stringify(user));
           resolve(user);
         })
         .catch(err => {
-          console.log(`error occurred`, err);
+          console.log('error occurred', err);
           reject(err);
         });
     });
