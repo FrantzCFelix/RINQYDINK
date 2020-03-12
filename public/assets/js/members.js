ef018182c0bf924caba5829f637e441f47c084c9
DDH <ddhoang1993@gmail.com>
2020-03-11 19:32:32 -0700

    make better visuals

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -1,18 +1,11 @@
 'use strict';
 
 $(document).ready(() => {
+
   $(`.parallax`).parallax();
-});
-
-$(document).ready(() => {
   $(`.modal`).modal();
-});
-
-$(document).ready(() => {
   $(`.sidenav`).sidenav();
-});
 
-$(document).ready(() => {
   $.get(`/api/user_data`).then(data => {
     $(`.member-name`).text(data.username);
   });
